e1117f2b09f7181c2f5bce29966663da1f2fe680
Greg Turbe <greturbe@gmail.com>
2024-02-15 20:55:20 -0500

    Added list containing 5 objects, added bootstrap button, added columns w red rectangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "./App.css";
+import Button from "react-bootstrap/esm/Button";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Gregory Turbe Hello
-		World
+                UD CISC275 with React Hooks and TypeScript - Gregory Turbe
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World
             </p>
+            <ul>
+                <li>Brown Bear</li>
+                <li>Black Bear</li>
+                <li>Polar bear</li>
+                <li>Panda bear</li>
+                <li>Sun bear</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
