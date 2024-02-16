714c72aba46b05ce9ecd57d33714481d8eefd8c8
Greg Turbe <greturbe@gmail.com>
2024-02-15 22:04:47 -0500

    changes background color of header, added image, altered imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import Button from "react-bootstrap/esm/Button";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import img from "src/photos/table_bear.jpg";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +13,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <img src="table_bear.jpg" alt="cool bear sitting at a table" />
             <ul>
                 <li>Brown Bear</li>
                 <li>Black Bear</li>
@@ -22,6 +24,22 @@ function App(): JSX.Element {
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        First Column
+                        <div>
+                            width:200px; height:500px; backgroundColor: #E22800
+                        </div>
+                    </Col>
+                    <Col>
+                        Second Column
+                        <div>
+                            width:200px; height:500px; backgroundColor: #E22800
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
