c912abb9cd8cde277b7a1d0ab83b45660ce84440
Greg Turbe <greturbe@gmail.com>
2024-02-15 22:19:41 -0500

    changed image, created element for red rectangle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import img from "src/photos/table_bear.jpg";
+import bear from "src/photos/table_bear.jpg";
 
 function App(): JSX.Element {
     return (
@@ -13,7 +13,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
-            <img src="table_bear.jpg" alt="cool bear sitting at a table" />
+            <img src={bear} alt="cool bear sitting at a table" />
             <ul>
                 <li>Brown Bear</li>
                 <li>Black Bear</li>
@@ -24,22 +24,14 @@ function App(): JSX.Element {
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        First Column
-                        <div>
-                            width:200px; height:500px; backgroundColor: #E22800
-                        </div>
-                    </Col>
-                    <Col>
-                        Second Column
-                        <div>
-                            width:200px; height:500px; backgroundColor: #E22800
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="App">
+                <Container>
+                    <Row>
+                        <Col className="red-rectangle">First Column</Col>
+                        <Col className="red-rectangle">Second Column</Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
