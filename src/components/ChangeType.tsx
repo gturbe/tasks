d0436c7e4b617368cb7fcc99a9c06a7316c81739
Greg Turbe <greturbe@gmail.com>
2024-03-03 09:56:03 -0500

    first pass ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,16 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function flipQuestionType(): void {
+        type === "short_answer_question"
+            ? "multiple_choice_question"
+            : "short_answer_question";
+    }
+    return (
+        <div>
+            Current Question Type:<span>{type}</span>
+            <Button onClick={flipQuestionType}>Change Type</Button>
+        </div>
+    );
 }
