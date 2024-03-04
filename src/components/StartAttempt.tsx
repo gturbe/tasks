d177bea68f94099d321c61ecc7cf78663ca30aec
Greg Turbe <greturbe@gmail.com>
2024-03-04 16:16:12 -0500

    first pass at StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,35 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, changeNumAttempts] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    function startQuiz(): void {
+        setProgress(true);
+        changeNumAttempts(attempts - 1);
+    }
+    function stopQuiz(): void {
+        setProgress(false);
+    }
+    function increaseAttempts(): void {
+        changeNumAttempts(attempts + 1);
+    }
+    return (
+        <div>
+            <Button
+                onClick={() => setProgress(true)}
+                disabled={inProgress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={() => setProgress(false)} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => changeNumAttempts(attempts + 1)}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
