d57cebe79af699424ce2c094a74c6d87c0698049
Greg Turbe <greturbe@gmail.com>
2024-03-16 23:25:22 -0400

    EditMode, multipleChoice, giveAttempts  pass all tests

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+const COLORS = [
+    "red",
+    "blue",
+    "yellow",
+    "green",
+    "orange",
+    "purple",
+    "magenta",
+    "cyan"
+];
 export function ChangeColor(): JSX.Element {
+    const [chosenColor, setChosonColor] = useState<string>(COLORS[0]);
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setChosonColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    inline
+                    type="radio"
+                    name="color"
+                    onChange={updateColor}
+                    id="color-select "
+                    label={color}
+                    value={color}
+                    checked={COLORS.includes(color)}
+                    key={color}
+                />
+            ))}
+            <div>You have chosen {chosenColor}</div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
 export function MultipleChoiceQuestion({
     options,
     expectedAnswer
@@ -7,9 +7,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedAnswer, setSelectedAnswer] = useState<string>(options[0]);
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>What is your favorite color?</Form.Label>
+                <Form.Select value={selectedAnswer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {selectedAnswer === expectedAnswer
+                ? "Correct Answer ✔️"
+                : "Incorrect Answer❌"}
         </div>
     );
 }
